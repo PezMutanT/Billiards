f30d4d2051517e7bd09d4ac0bfaf9fdae72ad5fa
PezMutanT <pezmutant@gmail.com>
2023-11-01 10:47:53 +0100

    Removed code from Cue that waited for several frames to stop the cue rigidbody.

--- Assets/Scripts/Cue.cs
+++ Assets/Scripts/Cue.cs
@@ -1,5 +1,3 @@
-using System;
-using Messaging;
 using UnityEngine;
 
 public class Cue : MonoBehaviour
@@ -9,31 +7,17 @@ public class Cue : MonoBehaviour
     [SerializeField] private Rigidbody _whiteBallRigidBody;
     [SerializeField] private float _distanceToWhiteBall;
     [SerializeField] private float _forceChargeOverTime;
-    [SerializeField] private int _numberOfFixedUpdatesToWaitBeforeStopping;
 
     private bool _isCharging = false;
     private bool _isShooting = false;
     private bool _hasHitWhiteBall = false;
     private float _forceMagnitude;
-    private int _elapsedFixedUpdatesSinceWhiteBallHit;
 
     public void Init()
     {
         _isCharging = false;
         _isShooting = false;
         _hasHitWhiteBall = false;
-        _elapsedFixedUpdatesSinceWhiteBallHit = 0;
-
-        Messenger.AddListener<BallStartedMoving>(OnBallStartedMoving);
-    }
-
-    private void OnBallStartedMoving(BallStartedMoving e)
-    {
-        /*if (e.Ball.BallType == BallType.White)
-        {
-            _hasHitWhiteBall = true;
-            _elapsedFixedUpdatesSinceWhiteBallHit = 0;
-        }*/
     }
 
     private void OnDrawGizmos()
@@ -61,24 +45,6 @@ public class Cue : MonoBehaviour
             Shoot();
         }
     }
-
-    /*private void FixedUpdate()
-    {
-        if (!_hasHitWhiteBall)
-        {
-            return;
-        }
-        
-        if (_elapsedFixedUpdatesSinceWhiteBallHit < _numberOfFixedUpdatesToWaitBeforeStopping)
-        {
-            _elapsedFixedUpdatesSinceWhiteBallHit++;
-        }
-        else
-        {
-            _hasHitWhiteBall = false;
-            _rigidBody.velocity = Vector3.zero;
-        }
-    }*/
 
     public void UpdateFromCamera(Vector3 cameraForwardVector)
     {
@@ -111,7 +77,6 @@ public class Cue : MonoBehaviour
         if (otherRigidBody != null && otherRigidBody == _whiteBallRigidBody)
         {
             _hasHitWhiteBall = true;
-            _elapsedFixedUpdatesSinceWhiteBallHit = 0;
             _rigidBody.velocity = Vector3.zero;
         }
     }
