dc6d85368fd5b1e4c19afb8ec1dc344e80ab49d4
PezMutanT <pezmutant@gmail.com>
2023-11-21 06:46:29 +0100

    Commented out noisy logs.

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -89,12 +89,12 @@ public class Ball : MonoBehaviour
         
         if (_previousVelocity == Vector3.zero && _rigidbody.velocity != Vector3.zero)
         {
-            Debug.Log($"Ball {name} started moving");
+            // Debug.Log($"Ball {name} started moving");
             Messenger.Send(new BallStartedMoving(this));
         }
         else if (_previousVelocity != Vector3.zero && _rigidbody.velocity == Vector3.zero)
         {
-            Debug.Log($"Ball {name} stopped moving");
+            // Debug.Log($"Ball {name} stopped moving");
             Messenger.Send(new BallStoppedMoving(this));
         }
 
