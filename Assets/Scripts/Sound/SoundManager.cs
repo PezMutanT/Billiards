73195f1f68ce4bfc3dcd89d776904082513f1360
PezMutanT <pezmutant@gmail.com>
2023-12-01 09:39:36 +0100

    Not playing sound for very low ball velocities and refactored code for choosing sound depending on velocity.

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,11 +1,25 @@
+using System;
 using System.Collections.Generic;
 using Messaging;
 using UnityEngine;
 
+[Serializable]
+public class AudioClipInfo
+{
+    public float MinBallVelocity;
+    public AudioClip AudioClip;
+
+    public AudioClipInfo(float minBallVelocity, AudioClip audioClip)
+    {
+        MinBallVelocity = minBallVelocity;
+        AudioClip = audioClip;
+    }
+}
+
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private List<AudioClip> _audioClips;
+    [SerializeField] private List<AudioClipInfo> _audioClips;
     
     public void Init()
     {
@@ -19,20 +33,31 @@ public class SoundManager : MonoBehaviour
 
     private void OnBallCollidedWithBall(BallCollidedWithBall msg)
     {
-        _audioSource.clip = GetClipDependingOnVelocity(msg.BallAVelocitySqrMagnitude);   
+        var audioClip = GetClipDependingOnVelocity(msg.BallAVelocitySqrMagnitude);
+        if (audioClip == null)
+        {
+            return;
+        }
+        
+        _audioSource.clip = audioClip;   
         
         _audioSource.Play();
     }
 
     private AudioClip GetClipDependingOnVelocity(float ballVelocitySqrMagnitude)
     {
-        //TODO 
-        
-        if (ballVelocitySqrMagnitude > 100f)
+        AudioClip resultAudioClip = null;
+        foreach (var audioClipInfo in _audioClips)
         {
-            return _audioClips[1];
+            if (ballVelocitySqrMagnitude >= audioClipInfo.MinBallVelocity)
+            {
+                resultAudioClip = audioClipInfo.AudioClip;
+                continue;
+            }
+
+            break;
         }
-        
-        return _audioClips[0];
+
+        return resultAudioClip;
     }
 }
