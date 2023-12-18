ef9691974747f65196933d73ed73cc37de50d0e4
PezMutanT <pezmutant@gmail.com>
2023-12-18 08:45:18 +0100

    Cinemachine test scene.

---
+++ Assets/Scenes/Cinemachine/CinemachinePlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachinePlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _playerRigidbody;
+    [SerializeField] private float _sensitivity;
+    
+    private float horizontalInput = 0f;
+    private float verticalInput = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        var direccion = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        
+        Vector3 movimiento = direccion * _sensitivity * Time.deltaTime;
+
+        transform.Translate(movimiento, Space.World);
+
+        CinemachineFreeLook a = new CinemachineFreeLook();
+        a.UpdateInputAxisProvider();
+    }
+}
