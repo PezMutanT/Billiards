8f1f777008f3eaf9fa9a81c5ff641c84dc20a079
PezMutanT <pezmutant@gmail.com>
2024-02-07 09:58:20 +0100

    File reorganization.

--- Assets/BallsConfiguration.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Balls configuration")]
-public class BallsConfiguration : ScriptableObject
-{
-    public Texture[] Balls;
-    public float HitStrength;
-}
